81c9be174458aa53833320dce83fc40bc0e4511a
NoobScience <noobscience123@gmail.com>
2025-01-03 01:26:56 +0530

    feat: Move from distance to similarity
    **Breaking Changes**
    
    * Introduces the concept of similarity instead of distance
    * All distances are now replaced with similarity
    * Threshold and similarity are now f64 to represent the similarity

--- src/levenshtein.rs
+++ src/levenshtein.rs
@@ -1,49 +1,72 @@
 /// Uses the [Levenshtein distance](https://en.wikipedia.org/wiki/Levenshtein_distance) algorithm to calculate the distance between two strings.
 /// The lower the distance, the better the correction.
-/// 
+///
+/// The Levenshtein Distance as a number is not a measure of string similarity.
+/// For similarity of string, use levenshtein_similarity.
+///
 /// The algorithm is implemented using a dynamic programming approach.
 /// In this, the distance between two strings is calculated by calculating the distance between the prefixes of the strings.
 /// Look at the wiki page for more information.
-/// 
+///
 /// This function is used by the Levenshtein algorithm in the Algorithm enum.
 /// You are free to use it in your own code, however, the most common use case is to use the correct function.
-/// 
+///
 /// # Arguments
-/// 
+///
 /// * `string1` - The first string to compare.
 /// * `string2` - The second string to compare.
-/// 
+///
 /// # Returns
-/// 
+///
 /// `u16` - The distance between the two strings.
-/// 
+///
 /// # Example
 /// ```
 /// use correct_word::levenshtein::levenshtein_distance;
-/// 
+///
 /// let distance = levenshtein_distance("hilo".to_string(), "hello".to_string());
 /// assert_eq!(distance, 2);
 /// ```
-pub fn levenshtein_distance(string1: String, string2: String)->u16{
-    if string1.len() < string2.len(){
+pub fn levenshtein_distance(string1: String, string2: String) -> u16 {
+    if string1.len() < string2.len() {
         return levenshtein_distance(string2, string1);
     }
 
-    if string2.is_empty(){
+    if string2.is_empty() {
         return string1.len() as u16;
     }
 
     let mut previous_row: Vec<usize> = (0..string2.len() + 1).collect();
-    for (i, c1) in string1.chars().enumerate(){
+    for (i, c1) in string1.chars().enumerate() {
         let mut current_row = vec![i + 1];
-        for (j, c2) in string2.chars().enumerate(){
+        for (j, c2) in string2.chars().enumerate() {
             let insertions = previous_row[j + 1] + 1;
             let deletions = current_row[j] + 1;
-            let substitutions = previous_row[j] + if c1 == c2 {0} else {1};
+            let substitutions = previous_row[j] + if c1 == c2 { 0 } else { 1 };
             current_row.push(insertions.min(deletions).min(substitutions));
         }
         previous_row = current_row;
     }
 
     previous_row[string2.len()] as u16
+}
+
+/// Uses the [Levenshtein distance](https://en.wikipedia.org/wiki/Levenshtein_distance) algorithm
+/// as a measure to calculate the similarity beyween two strings.
+/// Returns a f64 from 0 to 1: 1 being equal strings and 0 being completely different strings.
+///
+/// If you want to know if two strings are nearly equal; 0.5 is a good measure.
+/// This is what you want to use in most cases instead of calcuating distance.
+///
+/// # Arguments
+///
+/// * `string1` - The first string to compare.
+/// * `string2` - The second string to compare.
+///
+/// # Returns
+///
+/// `f64` - The distance between the two strings.
+pub fn levenshtein_similarity(string1: String, string2: String) -> f64 {
+    let distance = levenshtein_distance(string1.clone(), string2.clone());
+    1.0 - (distance as f64 / std::cmp::max(string1.len(), string2.len()) as f64)
 }
--- src/lib.rs
+++ src/lib.rs
@@ -4,11 +4,11 @@ pub mod levenshtein;
 /// A struct used to represent the result of the correct function.
 /// It has the word and the confidence of the correction.
 /// The word is an optional string, because the function might not be able to correct the word, given the threshold.
-/// The confidence is the distance between the input and the corrected word.
-/// The lower the distance, the better the correction.
-pub struct CorrectWord{
+/// The confidence is the similarity between the input and the corrected word between 0 to 1.
+/// The higher the similarity, the better the correction.
+pub struct CorrectWord {
     pub word: Option<String>,
-    pub confidence: u16,
+    pub confidence: f64,
 }
 
 /// # Enum: Algorithm
@@ -23,7 +23,7 @@ pub struct CorrectWord{
 /// use correct_word::correct_word;
 /// use correct_word::Algorithm;
 ///
-/// let result = correct_word(Algorithm::Levenshtein, "hilo".to_string(), vec!["hello".to_string(), "world".to_string()], Some(5));
+/// let result = correct_word(Algorithm::Levenshtein, "hilo".to_string(), vec!["hello".to_string(), "world".to_string()], None);
 /// assert_eq!(result.word.unwrap(), "hello");
 /// ```
 pub enum Algorithm {
@@ -41,16 +41,16 @@ pub enum Algorithm {
 ///
 /// # Returns
 ///
-/// `[CorrectWord](type.CorrectWord.html) = (Option<String>, u16))`
+/// `[CorrectWord](type.CorrectWord.html) = (Option<String>, f64))`
 ///
-/// This function returns a tuple of an optional string and a u16. The string is the corrected word, and the u16 is the distance between the input and the corrected word.
+/// This function returns a tuple of an optional string and a f64. The string is the corrected word, and the f64 is the similary between the input and the corrected word in the range from 0 to 1
 ///
 /// # Example
 /// ```
 /// use correct_word::correct_word;
 /// use correct_word::Algorithm;
 ///
-/// let result = correct_word(Algorithm::Levenshtein, "hilo".to_string(), vec!["hello".to_string(), "world".to_string()], Some(5));
+/// let result = correct_word(Algorithm::Levenshtein, "hilo".to_string(), vec!["hello".to_string(), "world".to_string()], Some(0.4));
 /// assert_eq!(result.word.unwrap(), "hello");
 /// ```
 ///
@@ -59,38 +59,38 @@ pub enum Algorithm {
 /// All the algorithms used in this crate return a distance between the input and the corrected word.
 /// The lower the distance, the better the correction.
 /// You can use the threshold to make sure that the function doesn't return a word that is too different from the input.
-/// By default, the threshold is 0, which means that the function will return the best option, even if it is very different from the input.
+/// By default, the threshold is 0.5.
 /// So, it is up to you to choose the level of tolerance you want.
-/// Usually, a threshold of 5 is a good value.
+/// Usually, a threshold of 0.5 is a good value which is the default.
 pub fn correct_word(
     algorithm: Algorithm,
     input: String,
     options: Vec<String>,
-    threshold: Option<u8>,
+    threshold: Option<f64>,
 ) -> CorrectWord {
     let mut best = String::new();
-    let mut best_distance = u16::MAX;
+    let mut best_now = 0.0;
     options.iter().for_each(|option| {
         let distance = match algorithm {
             Algorithm::Levenshtein => {
-                levenshtein::levenshtein_distance(input.to_string(), option.to_string())
+                levenshtein::levenshtein_similarity(input.to_string(), option.to_string())
             }
         };
-        if distance < best_distance {
+        if distance > best_now {
             best = option.to_string();
-            best_distance = distance;
+            best_now = distance;
         }
     });
 
-    if best_distance > threshold.unwrap_or(0) as u16 {
+    if best_now < threshold.unwrap_or(0.5) {
         CorrectWord {
             word: None,
-            confidence: best_distance,
+            confidence: best_now,
         }
     } else {
         CorrectWord {
             word: Some(best),
-            confidence: best_distance,
+            confidence: best_now,
         }
     }
 }
@@ -105,7 +105,7 @@ mod tests {
             Algorithm::Levenshtein,
             "he".to_string(),
             vec!["hello".to_string(), "world".to_string(), "hi".to_string()],
-            Some(5),
+            None,
         );
         assert_eq!(result.word.unwrap(), "hi");
     }
